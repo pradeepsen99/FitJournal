32d4c9fa7a1f863d4c421225ed36c137338af6ca
pradeepkumar <pradeepsen99@gmail.com>
2019-06-07 17:00:01 -0500

    Removed UI rotation and linked swift + storyboard for Meals

--- FitJournal/ViewControllers/MealsViewController.swift
+++ FitJournal/ViewControllers/MealsViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 Pradeep Kumar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MealsViewController: UIViewController {
+    
+    var settingsButton: UIBarButtonItem? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.topItem?.title = "Meals"
+        
+        settingsButton = UIBarButtonItem.init(
+            image: UIImage(named: "restaurant.png")?.withRenderingMode(.alwaysTemplate),
+            style: .plain,
+            target: self,
+            action: #selector(mealsButtonClick)
+        )
+        
+        self.navigationItem.rightBarButtonItem = settingsButton
+    }
+    
+    
+}
+
+extension MealsViewController{
+    @objc func mealsButtonClick(){
+        
+    }
+}
