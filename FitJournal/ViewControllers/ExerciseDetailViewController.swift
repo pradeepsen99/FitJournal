2a548007542ce29c6427bb4b440df0b274edaf1a
pradeepkumar <pradeepsen99@gmail.com>
2019-07-19 22:03:54 -0500

    Added Edit and Add Workout+Exercise View Controller

---
+++ FitJournal/ViewControllers/ExerciseDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ExerciseDetailViewController.swift
+//  FitJournal
+//
+//  Created by Pradeep Kumar on 7/19/19.
+//  Copyright © 2019 Pradeep Kumar. All rights reserved.
+//
+
+import Foundation
