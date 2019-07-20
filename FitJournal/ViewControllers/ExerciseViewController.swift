d5b2ac1ff7fc6d1ac7569e4bb6991f70e482690e
pradeepkumar <pradeepsen99@gmail.com>
2019-07-19 21:35:50 -0500

    Added TableView exercises

--- FitJournal/ViewControllers/ExerciseViewController.swift
+++ FitJournal/ViewControllers/ExerciseViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ExerciseViewController: UIViewController {
+class ExerciseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var displayArray = ["Bench Press","Squats","Deadlift"]
+    var myTableView: UITableView!
     var settingsButton: UIBarButtonItem? = nil
     
     override func viewDidLoad() {
@@ -25,6 +27,30 @@ class ExerciseViewController: UIViewController {
         )
         
         self.navigationItem.rightBarButtonItem = settingsButton
+        
+        tableSetup()
+    }
+    
+    func tableSetup(){
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height * 2
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(displayArray[indexPath.row])"
+        return cell
     }
     
     
