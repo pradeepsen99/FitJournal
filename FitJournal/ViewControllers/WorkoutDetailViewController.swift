58421f115c444a8b01528f9c36df88b73a352659
pradeepkumar <pradeepsen99@gmail.com>
2019-07-27 23:38:44 -0500

    Added back in WorkoutDetailViewController because forgot original meaning for it

---
+++ FitJournal/ViewControllers/WorkoutDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WorkoutDetailViewController.swift
+//  FitJournal
+//
+//  Created by Pradeep Kumar on 7/27/19.
+//  Copyright © 2019 Pradeep Kumar. All rights reserved.
+//
+// This class is the one run when you 
+
+import UIKit
+
+class WorkoutDetailViewController : UIViewController {
+    
+}
