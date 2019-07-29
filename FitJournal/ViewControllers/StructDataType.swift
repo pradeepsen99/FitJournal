bc3968ebdf102ea0c059219d8e814348ca87786e
pradeepkumar <pradeepsen99@gmail.com>
2019-07-28 23:17:59 -0500

    Added init for all the structs

--- FitJournal/ViewControllers/StructDataType.swift
+++ FitJournal/ViewControllers/StructDataType.swift
@@ -9,34 +9,63 @@
 struct Muscles : Codable{
     var name = ""
     var exercises = [Exercise]()
+    
+    init(nameInput: String, exercisesInput: [Exercise]) {
+        name = nameInput
+        exercises = exercisesInput
+    }
 }
 
 struct Exercise : Codable{
     var name = ""
     var id = 0
     var type = ""
-    var musclesWorked : [String]?
+    var musclesWorked : [Muscles]?
     var equipment : Equipment?
     var notes = ""
     var oneRM = 0
     var numberOfSets = 0
     var numofReps = 0
+    
+    init(nameInput: String, idInput: Int, typeInput: String, musclesWorkedInput: [Muscles], equipmentInput: Equipment, notesInput: String, oneRMInput: Int, numberOfSetsInput: Int, numOfRepsInput: Int) {
+        
+        name = nameInput
+        id = idInput
+        type = typeInput
+        musclesWorked = musclesWorkedInput
+        equipment = equipmentInput
+        notes = notesInput
+        oneRM = oneRMInput
+        numberOfSets = numberOfSetsInput
+        numofReps = numOfRepsInput
+        
+    }
 }
 
 struct Equipment : Codable {
     var name = ""
     var id = 0
+    
+    init(nameInput: String, idInput: Int) {
+        name = nameInput
+        id = idInput
+    }
 }
 
 struct Workout : Codable {
-    init(){
-        name = "test"
-    }
     var name = "test"
     var id = 0
     var exercises : [Exercise]?
     var numberOfAttempts = 0
     var notes = ""
+    
+    init(nameInput: String, idInput: Int, exercisesInput: [Exercise], numberOfAttemptsInput: Int, notesInput: String){
+        name = nameInput
+        id = idInput
+        exercises = exercisesInput
+        numberOfAttempts = numberOfAttemptsInput
+        notes = notesInput
+    }
 }
 
 struct Date : Codable {
@@ -63,4 +92,12 @@ struct WorkoutLog : Codable {
     var startTime : Date?
     var endTime : Date?
     var uniqueID = 0
+    
+    init(workoutInput: Workout, timeSpentMinInput: Double, startTimeInput: Date, endTimeInput: Date, uniqueIDInput: Int) {
+        workout = workoutInput
+        timeSpentMinutes = timeSpentMinInput
+        startTime = startTimeInput
+        endTime = endTimeInput
+        uniqueID = uniqueIDInput
+    }
 }
